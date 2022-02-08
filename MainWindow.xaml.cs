0b9647d90c2097391717ce21750567e44e9da72a
Kooni-Boop <kooni@knou.ac.kr>
2022-02-08 15:23:46 +0900

    initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NAudio;
+using NAudio.Wave;
+using NAudio.CoreAudioApi;
+using NAudio.Codecs;
+using static ArmyChat_TestFlight.WaveProvider;
+
+
+namespace ArmyChat_TestFlight
+{
+    public partial class MainWindow : Window
+    {
+        private WaveIn recorder;
+        private BufferedWaveProvider bufferedWaveProvider;
+        private SavingWaveProvider savingWaveProvider;
+        private WaveOut player;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            recorder = new WaveIn();
+            recorder.DataAvailable += RecorderOnDataAvailable;
+
+            bufferedWaveProvider = new BufferedWaveProvider(recorder.WaveFormat);
+            savingWaveProvider = new SavingWaveProvider(bufferedWaveProvider, "temp.wav");
+
+            player = new WaveOut();
+            player.Init(savingWaveProvider);
+
+            player.Play();
+            recorder.StartRecording();
+
+            
+        }
+
+        public void Checkvisibilty()
+        {
+            if (label2.Visibility == Visibility.Visible)
+            {
+                MessageBox.Show("Visible");
+            }
+        }
+
+        private void RecorderOnDataAvailable(object sender, WaveInEventArgs waveInEventArgs)
+        {
+            bufferedWaveProvider.AddSamples(waveInEventArgs.Buffer, 0, waveInEventArgs.BytesRecorded);
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            recorder.StopRecording();
+            player.Stop();
+            savingWaveProvider.Dispose();
+
+        }
+
+        private void OnButton_Click(object sender, RoutedEventArgs e)
+        {
+            label2.Visibility = Visibility.Visible;
+        }
+
+        private void OffButton_Click(object sender, RoutedEventArgs e)
+        {
+            label2.Visibility = Visibility.Hidden;
+        }
+
+        private void ChkButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WaveProvider.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using NAudio.Wave;
+
+namespace ArmyChat_TestFlight
+{
+    class WaveProvider
+    {
+        public class SavingWaveProvider : IWaveProvider, IDisposable
+        {
+            private readonly IWaveProvider sourceWaveProvider;
+            private readonly WaveFileWriter writer;
+            private bool isWriterDisposed;
+
+            private readonly MainWindow _mainWindow = new MainWindow();
+
+            public SavingWaveProvider(IWaveProvider sourceWaveProvider, string wavFilePath)
+            {
+                this.sourceWaveProvider = sourceWaveProvider;
+                writer = new WaveFileWriter(wavFilePath, sourceWaveProvider.WaveFormat);
+            }
+
+            public int Read(byte[] buffer, int offset, int count)
+            {
+                var read = sourceWaveProvider.Read(buffer, offset, count);
+                
+                if(read > 5)Debug.WriteLine("Number of Threads Exceeds Target Number");
+                if (count > 0 && !isWriterDisposed)
+                {
+                    writer.Write(buffer, offset, read);
+                }
+
+                if (count == 0)
+                {
+                    if (Windows.VisibilityProperty.Name == "returnval")
+                    {
+                        Console.WriteLine("123");
+                    }
+                    Dispose();
+                }
+
+                return read;
+            }
+
+            public class Windows : MainWindow
+            {
+                public void ReturnValue()
+                {
+                    label2.Visibility = Visibility.Hidden;
+                }
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            protected void Prop(string PropName)
+            {
+            }
+
+            public virtual string ReturnVal()
+            {
+                string val = "";
+
+                foreach (Window window in Application.Current.Windows)
+                {
+                    if (window.GetType() == typeof(MainWindow))
+                    {
+                        (window as MainWindow).label2.Visibility = Visibility.Hidden;
+                    }
+                }
+
+                if (_mainWindow.label2.Visibility == Visibility.Visible)
+                {
+                    MessageBox.Show("True");
+                    var a = 1;
+                    var b = 2;
+                    if (_mainWindow.Visibility == Visibility.Visible) b = a + b;
+                    Console.WriteLine(b);
+                    val = "true";
+                    
+
+                }
+
+                if (_mainWindow.label2.Visibility == Visibility.Hidden)
+                {
+                    MessageBox.Show("Invisible");
+                    val = "false";
+                }
+
+                return val;
+            }
+
+            public WaveFormat WaveFormat
+            {
+                get { return sourceWaveProvider.WaveFormat; }
+            }
+
+            public void Dispose()
+            {
+                if (!isWriterDisposed)
+                {
+                    isWriterDisposed = true;
+                    writer.Dispose();
+                }
+            }
+        }
+    }
+}
